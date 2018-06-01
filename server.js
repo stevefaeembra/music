ede462615ddfdf501f5f2a27828df48ceb678c8f
Steven Kay <stevendkay@gmail.com>
2018-06-01 19:11:53 +0100

    my god need to find out how to iterate properly over arrays

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const port = 3000;
 
 const app = express()
 
-app.get('/', (req, res) => res.send(music.names_for_notes([1,4,7])));
+// app.get(
+//   '/',(req, res) => 
+//   res.send(
+//     music.names_for_notes(
+//       music.notes_for_scale("c","major")
+//     ).join(",")
+//   )
+// );
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
+var m = music.names_for_notes(music.notes_for_scale("c#","major"))
+console.log(m)
