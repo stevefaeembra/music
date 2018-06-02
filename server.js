d9b1612063cf9f30a2c796412af7b2d73b662ca2
Steven Kay <stevendkay@gmail.com>
2018-06-02 19:54:36 +0100

    got a pug template working, returns bullet list.

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const music = require("./music.js")
 const express = require('express')
+format = require('python-format')
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const app = express()
+app.set('views','./views')
+app.set('view engine','pug')
+
+app.get('/', function (req, res) {
+  res.render('example', { 
+    title: 'Hey', 
+    notez: [0,4,7],   
+    notenames:['c','c#','d','d#','e','f','f#','g','g#','a','a#','b']})
+});
 
 app.get(
   '/:tonic/scale/:scale_name',(req, res) => 
@@ -30,6 +40,3 @@ app.get(
 );
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-//var m = music.names_for_notes(music.notes_for_scale("c#","major"))
-//console.log(m)
