ed2f33d696c86509472bc55cc51a6a9b27264050
Steven Kay <stevendkay@gmail.com>
2018-06-03 15:25:56 +0100

    fixed stylesheet, made relative to remove tonic part from url when fetching static css

--- server.js
+++ server.js
@@ -8,23 +8,24 @@ const port = 3000;
 const app = express()
 app.set('views','./views')
 app.set('view engine','pug')
-app.use(express.static('static'))
+app.use(express.static(__dirname + '/public'));
+//app.disable('etag'); // prevent 304 cacheing
 
-app.get('/', function (req, res) {
+app.get('/:tonic/:scale_name', function (req, res) {
   res.render('example', { 
-    title: 'Hey', 
-    notez: music.notes_for_scale("c","major"),  
+    title: 'Hey You', 
+    notez: music.notes_for_scale(req.params["tonic"],req.params["scale_name"]),  
     notenames:['c','c#','d','d#','e','f','f#','g','g#','a','a#','b']})
 });
 
-app.get(
-  '/:tonic/scale/:scale_name',(req, res) => 
-  res.send(
-    music.names_for_notes(
-      music.notes_for_scale(req.params["tonic"],req.params["scale_name"])
-    )
-  )
-);
+// app.get(
+//   '/:tonic/scale/:scale_name',(req, res) => 
+//   res.send(
+//     music.names_for_notes(
+//       music.notes_for_scale(req.params["tonic"],req.params["scale_name"])
+//     )
+//   )
+//);
 
 app.get(
   '/:tonic/scales',(req, res) => 
